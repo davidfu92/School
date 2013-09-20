d5017ee5fff12d3f41e1a3b3f387ca6e40e7de8a
David Fu <df328@null.cs.rutgers.edu>
2013-09-20 16:52:46 -0400

    rename funct EscapetoHext to printToken, and finish TKCreat

--- CS214/Proj1/tokenizer.c
+++ CS214/Proj1/tokenizer.c
@@ -2,13 +2,13 @@
  * tokenizer.c
  */
 #include <stdio.h>
+#include <stdlib.h>
 /*
  * Tokenizer type.  You need to fill in the type as part of your implementation.
  */
 
 struct TokenizerT_ {
-char* 
-
+	char **tokens;
 };
 
 typedef struct TokenizerT_ TokenizerT;
@@ -28,8 +28,46 @@ typedef struct TokenizerT_ TokenizerT;
  */
 
 TokenizerT *TKCreate(char *separators, char *ts) {
-
-  return NULL;
+	TokenizerT *token = malloc(sizeof(TokenizerT));
+	token->tokens =  malloc(100*sizeof(char*));  // create 100 token buffer
+	char *chararry =  calloc(100, sizeof(char)); // Creates a 100 character buffer and makes sure they are zero out
+	int word = 0, arr = 0;
+	char *temp = separators;
+	char sep = ' ';
+	char input = *ts;
+	if(input == '\0') {
+		free(token->tokens);
+		free(chararry);
+		free(token);
+		return NULL;
+	}
+	while(input != '\0') {
+		sep = *temp;
+		while(sep != '\0') {
+			if((input == sep) && (word == 0)) {
+				temp = separators;
+				break;
+			}
+			else if((input == sep) && (word>0)) {
+				temp = separators;
+				chararry = realloc(chararry, sizeof(char)*(word++));
+				*(chararry+word) = '\0';
+				word = 0;
+				token->tokens[arr] = chararry;
+				arr++;
+				chararry = malloc(100*sizeof(char));
+				break;
+			}
+			temp++;
+		}
+		if(sep == '\0') {
+			temp = separators;
+		}
+		input = *(ts++);
+	}
+	token->tokens = realloc(token->tokens, sizeof(char*)*(arr++));
+	token->tokens[arr] = NULL;
+	return token;
 }
 
 /*
@@ -60,12 +98,38 @@ char *TKGetNextToken(TokenizerT *tk) {
 }
 
 /*
- * EscapeToHex returns the reconfigure the character string return
- * by TKGetNextToken and takes the escape value and convert them
- * to their Hex value in the form 0x??
+ * printToken reconfigure the character string from
+ *  TKGetNextToken and takes the escape value and convert them
+ * to their Hex value in the form 0x?? and prints them
  */
-char *EscapeToHex(char *output) {
-	
+void printToken(char *output) {
+	char *il = output;
+	char c = *il;
+	while(c != '\0') {
+		if(c == '\\'){
+			c=*(il++);
+			switch(c) {
+				case 'n' : printf("[0x0a]");  
+					break;
+				case 'v' : printf("[0x0b]");
+					break;
+				case 'b' : printf("[0x08]");
+					break;
+				case 'r' : printf("[0x0d]");
+					break;
+				case 'f' : printf("[0x0c]");
+					break;
+				case 'a' : printf("[0x07]");
+					break;
+				DEFAULT : printf("%c",c);
+			}
+		} else {
+			printf("%c", c);
+		}
+		il++;
+		c=*il;
+	}
+	printf("\n");
 }
 /*
  * main will have two string arguments (in argv[1] and argv[2]).
@@ -76,14 +140,15 @@ char *EscapeToHex(char *output) {
  */
 
 int main(int argc, char **argv) {
-	char list[] ="hello mate";
+	char list[] ="hello\nmate";
 	char* points = list;
 	printf("%s This is Arg1\n",argv[1]);
 	printf("%s This is Arg2\n",argv[2]);
+	char *pointer = argv[2];
 	char c = ' ';
 	while(c!='\0') {
-		c = *points;
-		points++;
+		c = *pointer;
+		pointer++;
 		printf("%c\n", c);
 	}
 //	int x=0;
