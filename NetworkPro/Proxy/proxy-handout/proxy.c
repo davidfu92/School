28c0b30ebfd419c5a3879f7d08367cde0433a9ec
david.fu92 <david.fu92gmail.com>
2015-02-28 18:44:21 -0500

    proxy

---
+++ NetworkPro/Proxy/proxy-handout/proxy.c
@@ -0,0 +1,97 @@
+/*
+ * proxy.c - A Simple Sequential Web proxy
+ *
+ * Course Name: 14:332:456-Network Centric Programming
+ * Assignment 2
+ * Student Name:______________________
+ * 
+ * IMPORTANT: Give a high level description of your code here. You
+ * must also provide a header comment at the beginning of each
+ * function that describes what that function does.
+ */ 
+
+#include "csapp.h"
+
+/*
+ * Function prototypes
+ */
+void format_log_entry(char *logstring, struct sockaddr_in *sockaddr, char *uri, int size);
+
+
+int create_socket(int port) {
+	int server_sock, optval;
+	struct sockaddr_in server_addr;
+	if ((server_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		return SERVER_SOCKET_ERROR;
+	}
+	if (setsockopt(server_sock, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) < 0) {
+		return SERVER_SETSOCKOPT_ERROR;
+	}
+	memset(&server_addr, 0, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(port);
+	server_addr.sin_addr.s_addr = INADDR_ANY;
+	if (bind(server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) != 0) {
+		return SERVER_BIND_ERROR;
+	}
+	if (listen(server_sock, 20) < 0) {
+		return SERVER_LISTEN_ERROR;
+	}
+	return server_sock;
+}
+
+
+
+
+/* 
+ * main - Main routine for the proxy program 
+ */
+int main(int argc, char **argv)
+{
+
+    /* Check arguments */
+    if (argc != 2) {
+	fprintf(stderr, "Usage: %s <port number>\n", argv[0]);
+	exit(0);
+    }
+
+    exit(0);
+}
+
+/*
+ * format_log_entry - Create a formatted log entry in logstring. 
+ * 
+ * The inputs are the socket address of the requesting client
+ * (sockaddr), the URI from the request (uri), and the size in bytes
+ * of the response from the server (size).
+ */
+void format_log_entry(char *logstring, struct sockaddr_in *sockaddr, 
+		      char *uri, int size)
+{
+    time_t now;
+    char time_str[MAXLINE];
+    unsigned long host;
+    unsigned char a, b, c, d;
+
+    /* Get a formatted time string */
+    now = time(NULL);
+    strftime(time_str, MAXLINE, "%a %d %b %Y %H:%M:%S %Z", localtime(&now));
+
+    /* 
+     * Convert the IP address in network byte order to dotted decimal
+     * form. Note that we could have used inet_ntoa, but chose not to
+     * because inet_ntoa is a Class 3 thread unsafe function that
+     * returns a pointer to a static variable (Ch 13, CS:APP).
+     */
+    host = ntohl(sockaddr->sin_addr.s_addr);
+    a = host >> 24;
+    b = (host >> 16) & 0xff;
+    c = (host >> 8) & 0xff;
+    d = host & 0xff;
+
+
+    /* Return the formatted log entry string */
+    sprintf(logstring, "%s: %d.%d.%d.%d %s", time_str, a, b, c, d, uri);
+}
+
+
---
+++ NetworkPro/Proxy/proxy-handout/proxy.c~
@@ -0,0 +1,71 @@
+/*
+ * proxy.c - A Simple Sequential Web proxy
+ *
+ * Course Name: 14:332:456-Network Centric Programming
+ * Assignment 2
+ * Student Name:______________________
+ * 
+ * IMPORTANT: Give a high level description of your code here. You
+ * must also provide a header comment at the beginning of each
+ * function that describes what that function does.
+ */ 
+
+#include "csapp.h"
+
+/*
+ * Function prototypes
+ */
+void format_log_entry(char *logstring, struct sockaddr_in *sockaddr, char *uri, int size);
+
+/* 
+ * main - Main routine for the proxy program 
+ */
+int main(int argc, char **argv)
+{
+
+    /* Check arguments */
+    if (argc != 2) {
+	fprintf(stderr, "Usage: %s <port number>\n", argv[0]);
+	exit(0);
+    }
+
+    exit(0);
+}
+
+/*
+ * format_log_entry - Create a formatted log entry in logstring. 
+ * 
+ * The inputs are the socket address of the requesting client
+ * (sockaddr), the URI from the request (uri), and the size in bytes
+ * of the response from the server (size).
+ */
+void format_log_entry(char *logstring, struct sockaddr_in *sockaddr, 
+		      char *uri, int size)
+{
+    time_t now;
+    char time_str[MAXLINE];
+    unsigned long host;
+    unsigned char a, b, c, d;
+
+    /* Get a formatted time string */
+    now = time(NULL);
+    strftime(time_str, MAXLINE, "%a %d %b %Y %H:%M:%S %Z", localtime(&now));
+
+    /* 
+     * Convert the IP address in network byte order to dotted decimal
+     * form. Note that we could have used inet_ntoa, but chose not to
+     * because inet_ntoa is a Class 3 thread unsafe function that
+     * returns a pointer to a static variable (Ch 13, CS:APP).
+     */
+    host = ntohl(sockaddr->sin_addr.s_addr);
+    a = host >> 24;
+    b = (host >> 16) & 0xff;
+    c = (host >> 8) & 0xff;
+    d = host & 0xff;
+
+
+    /* Return the formatted log entry string */
+    sprintf(logstring, "%s: %d.%d.%d.%d %s", time_str, a, b, c, d, uri);
+}
+
+
